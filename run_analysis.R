3efe20deeade5768ef0a2b083bc7e5476d677d26
svoun <sinavo@yahoo.fr>
2014-09-20 17:14:35 +0000

    add run_analysis

--- run_analysis.R
+++ run_analysis.R
@@ -1,39 +1,69 @@
+# Getting and Cleaning Data Course Project
+# Merges the training and the test sets to create one data set.
+# Uses descriptive activity names to name the activities in the data set
+# Appropriately labels the data set with descriptive activity names.
+# Each person performed six activities (WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING)
+# Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 setwd("C:/Users/Silue/Desktop/getdata/UCI HAR Dataset")
-training = read.csv("UCI HAR Dataset/train/X_train.txt", sep="", header=FALSE)
-training[,562] = read.csv("UCI HAR Dataset/train/Y_train.txt", sep="", header=FALSE)
-training[,563] = read.csv("UCI HAR Dataset/train/subject_train.txt", sep="", header=FALSE)
-testing = read.csv("UCI HAR Dataset/test/X_test.txt", sep="", header=FALSE)
-testing[,562] = read.csv("UCI HAR Dataset/test/Y_test.txt", sep="", header=FALSE)
-testing[,563] = read.csv("UCI HAR Dataset/test/subject_test.txt", sep="", header=FALSE)
-activityLabels = read.csv("UCI HAR Dataset/activity_labels.txt", sep="", header=FALSE)
-# Read features and make the feature names better suited for R with some substitutions
-features = read.csv("UCI HAR Dataset/features.txt", sep="", header=FALSE)
-features[,2] = gsub('-mean', 'Mean', features[,2])
-features[,2] = gsub('-std', 'Std', features[,2])
-features[,2] = gsub('[-()]', '', features[,2])
-# Merge training and test sets together
-allData = rbind(training, testing)
-# Get only the data on mean and std. dev.
-colsWeWant <- grep(".*Mean.*|.*Std.*", features[,2])
-# First reduce the features table to what we want
-features <- features[colsWeWant,]
-# Now add the last two columns (subject and activity)
-colsWeWant <- c(colsWeWant, 562, 563)
-# And remove the unwanted columns from allData
-allData <- allData[,colsWeWant]
-# Add the column names (features) to allData
-colnames(allData) <- c(features$V2, "Activity", "Subject")
-colnames(allData) <- tolower(colnames(allData))
-currentActivity = 1
-for (currentActivityLabel in activityLabels$V2) {
-allData$activity <- gsub(currentActivity, currentActivityLabel, allData$activity)
-currentActivity <- currentActivity + 1
-}
-allData$activity <- as.factor(allData$activity)
-allData$subject <- as.factor(allData$subject)
-tidy = aggregate(allData, by=list(activity = allData$activity, subject=allData$subject), mean)
-# Remove the subject and activity column, since a mean of those has no use
-tidy[,90] = NULL
-tidy[,89] = NULL
-write.table(tidy, "tidy.txt", sep="\t")
+# load libraries
+library(reshape)
+library(plyr)
+# give them headings, and turn the numeric activities into something easier to read
+xTrain = read.table("./train/X_train.txt")
+yTrain = read.table("./train/y_train.txt")
+subjectTrain = read.table("./train/subject_train.txt")
+# test set
+xTest = read.table("./test/X_test.txt")
+yTest = read.table("./test/y_test.txt")
+subjectTest = read.table("./test/subject_test.txt")
+## Format x datasets (xTrain and xTest)
+# format variable names
+# load headings from file
+featuresdf = read.table("./features.txt")
+headings = featuresdf$V2
+# transfer headings to data set
+colnames(xTrain) = headings
+colnames(xTest) = headings
+### format y dataset (yTest and yTrain)
+# change V1 variable to something descriptive "activity"
+yTest <- rename(yTest, c(V1="activity"))
+yTrain <- rename(yTrain, c(V1="activity"))
+# change data values in yTest according to activity_labels.txt file
+# there are 6 activities
+activitydf  = read.table("./activity_labels.txt")
+# convert variable names to lowercase
+activityLabels = tolower(levels(activitydf$V2))
+# convert $activity to factor and add descriptive labels
+yTrain$activity = factor(
+yTrain$activity,
+labels = activityLabels
+)
+yTest$activity = factor(
+yTest$activity,
+labels = activityLabels
+)
+### Format subject variables (subject_train subject_test)
+# change subject variable name to be descriptive
+subjectTrain <- rename(subjectTrain, c(V1="subjectid"))
+subjectTest <- rename(subjectTest, c(V1="subjectid"))
+### Merge the training and the test sets to create one data set.
+# combine (x,y,subject) for the training and test sets
+train = cbind(xTrain, subjectTrain, yTrain)
+test = cbind(xTest, subjectTest, yTest)
+# combine train and test set
+fullData = rbind(train, test)
+### Data Extraction:
+# Extract only the measurements on the mean and standard deviation for each measurement.
+# keep the activity column as well
+pattern = "mean|std|subjectid|activity"
+tidyData = fullData[,grep(pattern , names(fullData), value=TRUE)]
+# tidy up variable names
+# Don't use underscores ( _ ) or hyphens ( - ) in identifiers
+# remove parentheses, dash, commas
+cleanNames = gsub("\\(|\\)|-|,", "", names(tidyData))
+names(tidyData) <- tolower(cleanNames)
+# summarize data
+result = ddply(tidyData, .(activity, subjectid), numcolwise(mean))
+# write file to output
+write.table(result, file="data.txt", sep = "\t", append=F)
 
